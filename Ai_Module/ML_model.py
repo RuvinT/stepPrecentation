41babb8653a1a7d3a503891cdd8c71f728dd5cc4
RuvinT <jagodaruvin@gmail.com>
2023-02-17 12:48:24 -0500

    Initial Commit
    Initial Commit with frontend backend and AI module

---
+++ Ai_Module/ML_model.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 12 22:58:43 2023
+
+@author: ruvinjagoda
+"""
+
+
+import numpy as np
+import pandas as pd
+import pandas_datareader as web
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import matplotlib.pyplot as plt
+import math
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.layers import Dense
+
+
+df = pd.read_csv('AAPL.csv')
+
+# Convert the 'Date' column to a datetime object
+df['Date'] = pd.to_datetime(df['Date'], format='%d-%m-%Y', dayfirst=True)
+
+# Filter the rows based on the 'Date' column
+df = df[df['Date'].dt.year >= 2016]
+
+
+
+plt.figure(figsize=(16, 8))
+plt.title( 'Close Price History')
+plt.plot (df[ 'Date'],df[ 'Close'])
+plt.xlabel( 'Date', fontsize=18)
+plt.ylabel ( "Close Price USD ($)")
+plt.show()
+
+#Create a new dataframe with only the 'Close column
+data = df. filter (['Close'])
+
+#Convert the dataframe to a numpy array
+dataset = data.values
+
+#Get the number of rows to train the model on
+training_data_len = math.ceil( len(dataset) * .8)
+#Scale the data
+scaler = MinMaxScaler (feature_range=(0,1))
+scaled_data = scaler.fit_transform(dataset)
+
+#Create the training data set
+#Create the scaled training data set
+train_data = scaled_data[0:training_data_len , :]
+#Split the data into x_train and y_train data sets
+
+# Create the scaled training data set
+train_data = scaled_data[0:int(training_data_len), :]
+# Split the data into x_train and y_train data sets
+x_train = []
+y_train = []
+
+for i in range(60, len(train_data)):
+    x_train.append(train_data[i-60:i, 0])
+    y_train.append(train_data[i, 0])
+    
+        
+# Convert the x_train and y_train to numpy arrays 
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+# Reshape the data
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+# x_train.shape  
+    
+model = Sequential()
+model.add(LSTM(units=50,return_sequences=True,input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50,return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50,return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1))
+model.compile(optimizer='adam',loss='mean_squared_error')
+model.fit(x_train,y_train,epochs=150,batch_size=32)    
+    
+    
+# Create the testing data set
+# Create a new array containing scaled values from index 1543 to 2002 
+test_data = scaled_data[training_data_len - 60: , :]
+# Create the data sets x_test and y_test
+x_test = []
+y_test = dataset[training_data_len:, :]
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
+    
+# Convert the data to a numpy array
+x_test = np.array(x_test)
+
+# Reshape the data
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1 ))
+
+# Get the models predicted price values 
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)
+
+# Get the root mean squared error (RMSE)
+rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
+print(rmse) 
+
+# Plot the data
+train = data[:training_data_len]
+valid = data[training_data_len:]
+train_d = df[ 'Date'][:training_data_len]
+valid_d = df[ 'Date'][training_data_len:]
+valid['Predictions'] = predictions
+# Visualize the data
+plt.figure(figsize=(16,6))
+plt.title('Model')
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Close Price USD ($)', fontsize=18)
+plt.plot(train_d,train['Close'])
+plt.plot(valid_d,valid[['Close', 'Predictions']])
+plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
+plt.show() 
+    
---
+++ Backend/pyRestAPI.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  1 01:47:44 2023
+
+@author: ruvinjagoda
+"""
+
+import mysql.connector
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Connect to database
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="Ruvin@123",
+  database="tempdatabase"
+)
+
+mycursor = mydb.cursor()
+
+@app.route("/stock_prices", methods=["GET"])
+def stock_prices():
+    mycursor.execute("SELECT * FROM stock_prices")
+    result = mycursor.fetchall()
+    stocks = [{"x": date, "open": open,"high":high,"low":low,"close":close,"volume": volume} for date, open, high, low, close, volume in result]
+    response = jsonify(stocks)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
